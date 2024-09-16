17ace58be53958eec0af1a26464b79fd93d2a118
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2024-09-16 15:24:20 +0530

    admin order page and finish the project

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -5,7 +5,7 @@ const orderSchema = new mongoose.Schema({
     items: { type: Array, required: true },
     amount: { type: Number, required: true },
     address: { type: Object, required: true },
-    status: { type: String, default: 'Food Precessing' },
+    status: { type: String, default: "Foode Processing"},
     date: { type: Date, default: Date.now() },
     payment: { type: Boolean, required: false }
 })
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authMiddleWare from '../middleware/auth.js';
-import { placeOrder, verifyOrder, userOrders, listOrders } from '../controllers/orderController.js';
+import { placeOrder, verifyOrder, userOrders, listOrders, updateStatus } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 
@@ -8,5 +8,6 @@ orderRouter.post("/place", authMiddleWare, placeOrder);
 orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleWare, userOrders)
 orderRouter.get("/list", listOrders)
+orderRouter.post("/status", updateStatus)
 
 export default orderRouter;
